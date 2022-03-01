75bb560eb9f32dd7c481b42b2e463dfc3e86fbb7
PamelaCleveland <pamelac021@gmail.com>
2022-03-01 03:01:30 -0500

    updates

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,6 +1,7 @@
 import React from "react";
 import resume from "../../assets/resume/Resume.pdf";
 import working from "../../assets/images/--about.aee0f771.png"
+import badge from "../../assets/images/coding-certificate SM.png"
 
 //import coding from "../../assets/images/development.png"
 //<img src={coding} alt="skills"/>
@@ -13,18 +14,26 @@ const Resume = () => (
         <div className="skills-wrapper">
             <div className="skills">
                 <ul>
-                    <li>HTML/ CSS/ Bootstrap</li>
                     <li>Git</li>
-                    <li>JavaScript/ jQuery</li>
+                    <li>MERN</li>
+                    <li>MySQL</li>
+                    <li>Sequelize ORM/ Mongoose ODM</li>
+                    <li>React</li>
+                    <li>NoSQL (MongoDB)</li>
+                    <li>GraphQL/ Apollo Server</li>
+                    <li>Node.js</li>
+                    <li>Progressive Web Applications</li>
+                    <li>Object-oriented Programming</li>
+                    <li>Express.js</li>
+                    <li>Data Structures</li>
+                    <li>Database Management</li>
+                    <li>Client-Side Storage</li>
                     <li>REST APIs</li>
                     <li>Responsive Design</li>
-                    <li>React</li>
-                    <li>Handlebars</li>
-                    <li>Node.js/ Express.js</li>
-                    <li>Express</li>
-                    <li>MySQL/ Sequelize</li>
-                    <li>MongoDB/ Mongoose</li>
-                    <li>GraphQL/ Apollo Server</li>
+                    <li>Agile</li>  
+                    <li>HTML/ CSS/ JavaScript</li>
+                    <li><a href="https://www.credly.com/badges/d484a158-8c89-42c4-ae0b-15d3e196207f/public_url" target="_blank" rel="noopener noreferrer"><img src={badge} alt="UNCC coding badge" /></a>
+                        </li>
                 </ul>
             </div>
         </div>
--- src/components/pages/Work.js
+++ src/components/pages/Work.js
@@ -1,11 +1,15 @@
 import React from "react";
 import budget from "../../assets/images/budget-tracker.png"
-import weather from "../../assets/images/weather-dashboard.png"
+//import weather from "../../assets/images/weather-dashboard.png"
 import eCommerce from "../../assets/images/ecom.jpg"
 //import noteTaker from "../../assets/images/note-taker.png"
-import healthApp from "../../assets/images/health.png"
+import healthApp from "../../assets/images/medicine-healthcare.gif"
 import social from "../../assets/images/social.jpg"
-import newToTown from "../../assets/images/new-to-town.png"
+//import newToTown from "../../assets/images/new-to-town.png"
+import techblog from "../../assets/images/techblog.jpg"
+import books from "../../assets/images/bookshelf.png"
+//import ??? from "../../assets/images/???"
+
 
 const Work = () => (
     <section>
@@ -13,27 +17,16 @@ const Work = () => (
             <h1>Projects</h1>
         </div>
         <div className="projects row justify-content-center">
-            {/* Budget Tracker */}
+
+            {/* Tech Blog */}
             <div className="project-items col-xl-4 col-sm-8">
-                <img src={budget} alt="budget-tracker"/>
+                <img src={techblog} alt="blogging" />
                 <div className="image-text">
-                    <h4>Budget Tracker</h4>
-                    <p>PWA</p>
-                    <a href="https://github.com/pamelac21/Budget-Tracker" target="_blank"
+                    <h4>Tech Blog</h4>
+                    <p>SQL - Sole Author</p>
+                    <a href="https://github.com/pamelac21/Tech-Blog" target="_blank"
                         rel="noopener noreferrer"><i class="bi bi-code"></i> Source Code</a>
-                    <a href="https://polar-hollows-87169.herokuapp.com/" target="_blank"
-                        rel="noopener noreferrer"><i class="bi bi-app"></i> Live Application</a>
-                </div>
-            </div>
-            {/* Weather Dashboard */}
-            <div className="project-items col-xl-4 col-sm-8">
-                <img src={weather} alt="weather-dashboard" />
-                <div className="image-text">
-                    <h4>Weather Dashboard</h4>
-                    <p>Full-Stack Application</p>
-                    <a href="https://github.com/pamelac21/Weather-Dashboard" target="_blank"
-                        rel="noopener noreferrer"><i class="bi bi-code"></i> Source Code</a>
-                    <a href="https://pamelac21.github.io/Weather-Dashboard/" target="_blank"
+                    <a href="https://secure-sands-16724.herokuapp.com/" target="_blank"
                         rel="noopener noreferrer"><i class="bi bi-app"></i> Live Application</a>
                 </div>
             </div>
@@ -42,7 +35,7 @@ const Work = () => (
                 <img src={eCommerce} alt="computer"/>
                 <div className="image-text">
                     <h4>E-Commerce Back-End</h4>
-                    <p>ORM</p>
+                    <p>SQL - Refactor</p>
                     <a href="https://github.com/pamelac21/E-commerce" target="_blank"
                         rel="noopener noreferrer"><i class="bi bi-code"></i> Source Code</a>
                 </div>
@@ -52,7 +45,7 @@ const Work = () => (
                 <img src={healthApp} alt="note-taker"/>
                 <div className="image-text">
                     <h4>Health Care App</h4>
-                    <p>MERN</p>
+                    <p>MERN - Back-End/Authentication</p>
                     <a href="https://github.com/crhodes111/Healthcare-Project-3" target="_blank"
                         rel="noopener noreferrer"><i class="bi bi-code"></i> Source Code</a>
                     <a href="https://quiet-dawn-07208.herokuapp.com/" target="_blank"
@@ -64,20 +57,32 @@ const Work = () => (
                 <img src={social} alt="social-network"/>
                 <div className="image-text">
                     <h4>Social Network API</h4>
-                    <p>MongoDB</p>
+                    <p>MongoDB - Sole Author</p>
                     <a href="https://github.com/pamelac21/Social-Network-API" target="_blank"
                         rel="noopener noreferrer"><i class="bi bi-code"></i> Source Code</a>
                 </div>
             </div>
-            {/* New To Town */}
+            {/* Book Search Engine */}
             <div className="project-items col-xl-4 col-sm-8">
-                <img src={newToTown} alt="new-to-town"/>
+                <img src={books} alt="some books"/>
                 <div className="image-text">
-                    <h4>New To Town</h4>
-                    <p>Server Side APIs</p>
-                    <a href="https://github.com/pamelac21/Project-1-APIs" target="_blank"
+                    <h4>Book Search Engine</h4>
+                    <p>MERN - Refactor</p>
+                    <a href="https://github.com/pamelac21/Book-Search-Engine" target="_blank"
                         rel="noopener noreferrer"><i class="bi bi-code"></i> Source Code</a>
-                    <a href="https://shagens.github.io/New-To-Town/" target="_blank"
+                    <a href="https://limitless-sea-64444.herokuapp.com/" target="_blank"
+                        rel="noopener noreferrer"><i class="bi bi-app"></i> Live Application</a>
+                </div>
+            </div>
+            {/* Budget Tracker */}
+            <div className="project-items col-xl-4 col-sm-8">
+                <img src={budget} alt="budget-tracker"/>
+                <div className="image-text">
+                    <h4>Budget Tracker</h4>
+                    <p>NoSQL - Refactor</p>
+                    <a href="https://github.com/pamelac21/Budget-Tracker" target="_blank"
+                        rel="noopener noreferrer"><i class="bi bi-code"></i> Source Code</a>
+                    <a href="https://polar-hollows-87169.herokuapp.com/" target="_blank"
                         rel="noopener noreferrer"><i class="bi bi-app"></i> Live Application</a>
                 </div>
             </div>
@@ -87,3 +92,53 @@ const Work = () => (
 
 
 export default Work;
+
+//
+//             {/* Budget Tracker */}
+//             <div className="project-items col-xl-4 col-sm-8">
+//                 <img src={budget} alt="budget-tracker"/>
+//                 <div className="image-text">
+//                     <h4>Budget Tracker</h4>
+//                     <p>PWA</p>
+//                     <a href="https://github.com/pamelac21/Budget-Tracker" target="_blank"
+//                         rel="noopener noreferrer"><i class="bi bi-code"></i> Source Code</a>
+//                     <a href="https://polar-hollows-87169.herokuapp.com/" target="_blank"
+//                         rel="noopener noreferrer"><i class="bi bi-app"></i> Live Application</a>
+//                 </div>
+//             </div>
+
+//             {/* Weather Dashboard */}
+//             <div className="project-items col-xl-4 col-sm-8">
+//                 <img src={weather} alt="weather-dashboard" />
+//                 <div className="image-text">
+//                     <h4>Weather Dashboard</h4>
+//                     <p>Full-Stack Application</p>
+//                     <a href="https://github.com/pamelac21/Weather-Dashboard" target="_blank"
+//                         rel="noopener noreferrer"><i class="bi bi-code"></i> Source Code</a>
+//                     <a href="https://pamelac21.github.io/Weather-Dashboard/" target="_blank"
+//                         rel="noopener noreferrer"><i class="bi bi-app"></i> Live Application</a>
+//                 </div>
+//             </div>
+
+//             {/* New To Town */}
+//             <div className="project-items col-xl-4 col-sm-8">
+//                 <img src={newToTown} alt="new-to-town"/>
+//                 <div className="image-text">
+//                     <h4>New To Town</h4>
+//                     <p>Server Side APIs</p>
+//                     <a href="https://github.com/pamelac21/Project-1-APIs" target="_blank"
+//                         rel="noopener noreferrer"><i class="bi bi-code"></i> Source Code</a>
+//                     <a href="https://shagens.github.io/New-To-Town/" target="_blank"
+//                         rel="noopener noreferrer"><i class="bi bi-app"></i> Live Application</a>
+//                 </div>
+//             </div>
+            // {/* Social Network API */}
+            // <div className="project-items col-xl-4 col-sm-8">
+            //     <img src={social} alt="social-network"/>
+            //     <div className="image-text">
+            //         <h4>Social Network API</h4>
+            //         <p>MongoDB</p>
+            //         <a href="https://github.com/pamelac21/Social-Network-API" target="_blank"
+            //             rel="noopener noreferrer"><i class="bi bi-code"></i> Source Code</a>
+            //     </div>
+            // </div>
